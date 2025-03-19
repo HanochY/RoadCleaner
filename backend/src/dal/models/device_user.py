ddb9f04967d87db69b2921bef967cc7c5c7eee46
hanochyarkoni <hanochyarkoni@gmail.com>
2025-03-19 12:29:18 +0200

    add device user pasword enc

--- backend/src/dal/models/device_user.py
+++ backend/src/dal/models/device_user.py
@@ -1,8 +1,9 @@
 from dal.models._base import Common
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, mapped_column
+from api.main.security.encrypted_column import Encrypted
 
 class DeviceUser(Common):
     __tablename__ = 'user'
     
     name: Mapped[str]
-    password: Mapped[str]
+    password: Mapped[str] = mapped_column(Encrypted(str))
